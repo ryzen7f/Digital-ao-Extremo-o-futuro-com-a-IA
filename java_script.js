27c23ad8d24dbae7055693217dd86bded41f3188
DEV.DAVI <ryzen7f@gmail.com>
2026-08-18 19:48:52 -0300

    animações do principal

--- java_script.js
+++ java_script.js
@@ -3,3 +3,17 @@ function abrirMenu() {
 
     nav.classList.toggle("ativo");
 }
+
+const elementos = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+
+    elementos.forEach((elemento) => {
+
+        if (elemento.getBoundingClientRect().top < window.innerHeight) {
+            elemento.classList.add("ativo");
+        }
+
+    });
+
+});
